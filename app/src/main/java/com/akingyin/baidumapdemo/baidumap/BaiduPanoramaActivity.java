9cce9a2ba0f0e72a8dabdc301591b653dd953bfa
akingyin1987 <akingyin@163.com>
2016-01-21 23:43:15 +0800

    加入百度全景图，但进入全景图会黑屏幕

--- app/src/main/java/com/akingyin/baidumapdemo/baidumap/BaiduPanoramaActivity.java
+++ app/src/main/java/com/akingyin/baidumapdemo/baidumap/BaiduPanoramaActivity.java
@@ -1,6 +1,7 @@
 package com.akingyin.baidumapdemo.baidumap;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -24,32 +25,46 @@ public class BaiduPanoramaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baidu_panorama);
         mPanoView = (PanoramaView)findViewById(R.id.panorama);
+        mPanoView.setShowTopoLink(true);
         double  lat = getIntent().getDoubleExtra("lat",0);
         double  lng = getIntent().getDoubleExtra("lng",0);
+
+
         imageMarker = new ImageMarker();
-        imageMarker.setMarkerPosition(new Point(lat,lng));
+        imageMarker.setMarkerPosition(new Point(lat, lng));
         imageMarker.setMarkerHeight(2.6f);
         imageMarker.setMarker(getResources().getDrawable(R.drawable.icon_marka));
         mPanoView.addMarker(imageMarker);
-        mPanoView.setShowTopoLink(true);
-        mPanoView.setPanorama(lng, lat);
+
         mPanoView.setPanoramaViewListener(new PanoramaViewListener() {
             @Override
             public void onLoadPanoramaBegin() {
-
+                System.out.println("onLoadPanoramaBegin");
             }
 
             @Override
             public void onLoadPanoramaEnd(String s) {
-
+                System.out.println("s=" + s);
             }
 
             @Override
-            public void onLoadPanoramaError(String s) {
-                Toast.makeText(BaiduPanoramaActivity.this,s,Toast.LENGTH_SHORT).show();
+            public void onLoadPanoramaError(final String s) {
+                System.out.println("onLoadPanoramaError="+s);
             }
         });
+        loadLatlng(lat, lng);
     }
+
+    public   void  loadLatlng(final  double lat,final  double lng){
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                mPanoView.setPanorama(lng, lat);
+            }
+        }.start();
+    }
+
 
     private void initBMapManager() {
         DemoApp app = (DemoApp) this.getApplication();
